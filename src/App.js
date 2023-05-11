f66f9ccf14124e63bc8f5e41e7b7f910af967c4b
Prabeen Bahinipati <131144558+Prabeen-Kumar-PK@users.noreply.github.com>
2023-05-11 12:54:28 +0530

    before update function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
- import './App.css';
+import './App.css';
 
 
- import React, { Component } from 'react'
+import React, { Component } from 'react'
 import Nav from './Components/Nav';
 import News from './Components/News';
- 
- export default class App extends Component {
-   render() {
-     return (
-       <div>
-        {/* this is a class based Component */}
-        <Nav/>
-        <News/>
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
 
-       </div>
-     )
-   }
- }
- 
+} from "react-router-dom"
+
+
+export default class App extends Component {
+  render() {
+    return (
+      // pageNumber=5
+      <div>
+        {/* this is a class based Component */}
+        <Router>
+          <Nav />
+
+          {/* <News  country={"in"} category={"science"} pageSize={9}/> */}
+
+          <Routes>
+            <Route exact path="/home" element={<News key={"home"} country={"in"} category={"general"} pageSize={9} />} />
+            <Route exact path="/general" element={<News key={"general"} country={"in"} category={"general"} pageSize={9} />} />
+            <Route exact path="/science" element={<News key={"science"} country={"in"} category={"science"} pageSize={9} />} />
+            <Route exact path="/health" element={<News key={"health"} country={"in"} category={"health"} pageSize={9} />} />
+            <Route exact path="/technology" element={<News key={"technology"} country={"in"} category={"technology"} pageSize={9} />} />
+            <Route exact path="/sports" element={<News key={"sports"} country={"in"} category={"sports"} pageSize={9} />} />
+            <Route exact path="/entertainment" element={<News key={"entertainment"} country={"in"} category={"entertainment"} pageSize={9} />} />
+            <Route exact path="/business" element={<News key={"business"} country={"in"} category={"business"} pageSize={9} />} />
+
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+}
+
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
-// import PropTypes from 'prop-types'
+import Loading from "./Loading";
+import PropTypes from 'prop-types'
 
-export class news extends Component {
-  //   static propTypes = {
-
-  //   }
+export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    category: "general",
+    pageSize: 6
+  }
+  static propTypes = {
+    country: PropTypes.string.isRequired,
+    category: PropTypes.string,
+    pageSize: PropTypes.number
+  }
 
   // article variable from sample.json
   a = [];
@@ -17,8 +25,8 @@ export class news extends Component {
     this.state = {
       a: this.a,
       loading: false,
-      page:1
-      
+      page: 1
+
     };
   }
 
@@ -26,53 +34,54 @@ export class news extends Component {
 
   // Concept of Component DId Mount
   async componentDidMount() {
-    let apUrl =`https://newsapi.org/v2/top-headlines?country=in&apiKey=6c9e8b6f27664b8286e21d73ec60ee2a&page=${this.state.page}`;
-
+   const apUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6c9e8b6f27664b8286e21d73ec60ee2a&page=${this.state.page}&pagesize=${this.props.pageSize}`;
+    this.setState({ loading: true })
     let data = await fetch(apUrl);
+    this.setState({ loading: true })
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       a: parsedData.articles,
-      loading:false,
-      page:this.state.page ,
-      totalArticles:parsedData.totalResults
+      loading: false,
+      page: this.state.page,
+      totalArticles: parsedData.totalResults
 
     });
 
   }
 
 
-   handlePrevClick= async ()=>{
-    let apUrl = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c9e8b6f27664b8286e21d73ec60ee2a&page=${this.state.page-1}`;
-
+  handlePrevClick = async () => {
+   const apUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6c9e8b6f27664b8286e21d73ec60ee2a&page=${this.state.page - 1}&pagesize=${this.props.pageSize}`;
+    this.setState({ loading: true })
     let data = await fetch(apUrl);
+    this.setState({ loading: true })
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       a: parsedData.articles,
-      loading:false,
-      page:this.state.page - 1,
-      totalArticles:parsedData.totalResults
+      loading: false,
+      page: this.state.page - 1,
+      totalArticles: parsedData.totalResults
 
     });
   }
-  
-  handleNextClick= async ()=>{
 
-    if(this.state.page+1>Math.ceil(this.state.totalArticles/20)){
+  handleNextClick = async () => {
 
-    }else{
-      let apUrl = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6c9e8b6f27664b8286e21d73ec60ee2a&page=${this.state.page+1}`;
-
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize))) {
+     const apUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6c9e8b6f27664b8286e21d73ec60ee2a&page=${this.state.page + 1}&pagesize=${this.props.pageSize}`;
+      this.setState({ loading: true })
       let data = await fetch(apUrl);
+      this.setState({ loading: true })
       let parsedData = await data.json();
       console.log(parsedData);
       this.setState({
         a: parsedData.articles,
-        loading:false,
-        page:this.state.page + 1,
-        totalArticles:parsedData.totalResults
-  
+        loading: false,
+        page: this.state.page + 1,
+        totalArticles: parsedData.totalResults
+
       });
     }
 
@@ -83,13 +92,20 @@ export class news extends Component {
       <div>
         <div className="container my-3">
           <h1 className="text-center">NewsLine - Top Headlines</h1>
+          {this.state.loading && <Loading />}
+
+
+         
+
           <div className="row">
-            {this.state.a.map((element) => {
+            {!this.state.loading && this.state.a.map((element) => {
+
+
               // console.log(element);
               return (
 
                 <div className="col-md-4" key={element.url}>
-                  <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imgUrl={!element.urlToImage ? `https://images.creativemarket.com/0.1.0/ps/3373652/1160/772/m1/fpnw/wm1/breaking_news_1_scr-.jpg?1507354627&s=58989dc4123b369f1b12fda40a5a5207` : element.urlToImage} newsUrl={!element.url ? "" : element.url} />
+                  <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imgUrl={!element.urlToImage ? `https://images.creativemarket.com/0.1.0/ps/3373652/1160/772/m1/fpnw/wm1/breaking_news_1_scr-.jpg?1507354627&s=58989dc4123b369f1b12fda40a5a5207` : element.urlToImage} newsUrl={element.url} Author={!element.author ? "Unknown" : element.author} date={element.publishedAt} source={element.source.name} />
                 </div>
 
               )
@@ -97,9 +113,9 @@ export class news extends Component {
           </div>
           <div className="d-flex justify-content-between">
 
-            <button disabled={this.state.page<=1} className="btn  btn-secondary" onClick={this.handlePrevClick}>&#11160;</button>
+            <button disabled={this.state.page <= 1} className="btn  btn-secondary" onClick={this.handlePrevClick}>&#11160;</button>
 
-            <button  className="btn  btn-primary" onClick={this.handleNextClick}>&#11162;</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize)} className="btn  btn-primary" onClick={this.handleNextClick}>&#11162;</button>
 
           </div>
         </div>
@@ -108,4 +124,22 @@ export class news extends Component {
   }
 }
 
-export default news;
+export default News;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// image
+// `https://images.creativemarket.com/0.1.0/ps/3373652/1160/772/m1/fpnw/wm1/breaking_news_1_scr-.jpg?1507354627&s=58989dc4123b369f1b12fda40a5a5207`
