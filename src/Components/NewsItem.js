0133421452383967709ac6f00e051fe7bd500d7c
Prabeen Bahinipati <131144558+Prabeen-Kumar-PK@users.noreply.github.com>
2023-05-14 11:57:38 +0530

    completed but mode pending

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -4,7 +4,7 @@ const NewsItem =(props)=> {
   
 
 
-    let { title, description, imgUrl, Author, date, newsUrl, source } = props
+    let { title, description, imgUrl, Author, date, newsUrl} = props
 
 
     return (
@@ -14,16 +14,19 @@ const NewsItem =(props)=> {
 
           <img src={imgUrl} className="card-img-top" alt={title} />
 
-          <div className='d-flex position-absolute justify-content-end  ' style={{right:0}}>
-           <span className=" badge rounded-pill bg-danger" style={{zIndex:1, left:"86%", top:5}}>
-              {source}
-            </span>
-           </div>
+          
 
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <h6 className="card-text"><small className="text-muted">By {Author} on {new Date(date).toGMTString()}</small></h6>
+
+            {/* <div className='d-flex position-absolute justify-content-end  ' style={{right:0}}>
+           <span className=" badge rounded-pill bg-danger" style={{zIndex:1, left:"86%", top:5}}>
+              {source}
+            </span>
+           </div> */}
+
             <a rel="norefereer" href={newsUrl} className="btn btn-primary" style={{backgroundColor:"#070A52"}} >Read More</a>
           </div>
 
